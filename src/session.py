3d8645ea97b8630b3cef6064e908e63375c56bc6
wmahan <wmahan@marvin>
2010-03-16 23:59:23 -0400

    .

--- src/session.py
+++ src/session.py
@@ -60,7 +60,7 @@ class Session(object):
     def set_ivars_from_str(self, s):
         """Parse a %b string sent by Jin to set ivars before logging in."""
         for (i, val) in enumerate(s):
-            self.ivars[var.ivar_number[i]] = val
+            self.ivars[var.ivar_number[i].name] = val
         self.conn.write(_("Ivars set.\n"))
     
     def set_ivar(self, v, val):
