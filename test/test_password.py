2fcce6ac2055357a91d37056a70b570f846c783b
Wil Mahan <wmahan+bitbucket@gmail.com>
2010-09-20 23:20:12 -0400

    .

--- test/test_password.py
+++ test/test_password.py
@@ -23,7 +23,7 @@ class TestPassword(Test):
         t = self.connect_as_guest()
         t.write('password foo bar\r\n')
         self.expect("only for registered", t)
-        t.close()
+        self.close(t)
 
         t = self.connect_as_admin()
         t.write('password wrongpass test\r\n')
@@ -31,12 +31,12 @@ class TestPassword(Test):
 
         t.write('password %s test\r\n' % admin_passwd)
         self.expect("changed", t)
-        t.close()
+        self.close(t)
 
         t = self.connect()
         t.write("admin\r\ntest\r\n")
         self.expect("fics%", t)
         t.write("password test %s\r\n" % admin_passwd)
-        t.close()
+        self.close(t)
 
 # vim: expandtab tabstop=4 softtabstop=4 shiftwidth=4 smarttab autoindent
