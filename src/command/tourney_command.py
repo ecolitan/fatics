7205b1287804105f82cd2ccb57c98a64e3316bed
IMSA Student <IMSA Student@cyberquiz1.gateway.2wire.net>
2011-08-10 09:22:28 -0500

    Enabling users to join tourneys

--- src/command/tourney_command.py
+++ src/command/tourney_command.py
@@ -24,15 +24,15 @@ import user
 
 from command import *
 
-@ics_command('tourneylist', '', admin.Level.user)
-class Tourneylist(Command):
+@ics_command('listtourneys', '', admin.Level.user)
+class Listtourneys(Command):
     def run(self, args, conn):
         if conn.user.is_guest:
             conn.write("Only registered players may use tourney commands.")
             return
         conn.write(' -----------------------------------------------------------------------\n')
         conn.write('| Current Tournaments                                                   |\n')
-        conn.write('|-----------------------------------------------------------------------|')
+        conn.write('|-----------------------------------------------------------------------|\n')
         conn.write('| ID  | Tourney Name                | T.Ctrl. | Pair. | Manager         |\n')
         conn.write(' -----------------------------------------------------------------------\n')
         for t in tourney.tourneys:
@@ -52,6 +52,31 @@ class Createtourney(Command):
         number = tourney.tourneys.index(tournament)
         tournament.manager = conn.user.name
         conn.write("New tournament created with ID %d.\n" % number)
+
+@ics_command('jointourney', 'd', admin.Level.user)
+class Jointourney(Command):
+    def run(self, args, conn):
+        number = int(args[0])
+        if number >= len(tourney.tourneys):
+            conn.write('Tourney number %d not found.\n' % number)
+            return
+        tourney.tourneys[number].players.append(conn.user.name)
+        tourney.tourneys[number].player_scores[conn.user.name] = 0
+        time_control = tourney.tourneys[number].time_control.split(" ")
+        time_minute = int(time_control[0])
+        time_second = int(time_control[1])
+        # no variants yet
+        if time_minute > 10:
+            tourney.tourneys[number].player_ratings[conn.user.name] = conn.user.get_rating('standard')
+        elif time_minute > 2:
+            tourney.tourneys[number].player_ratings[conn.user.name] = conn.user.get_rating('blitz')
+        elif time_minute <= 2:
+            tourney.tourneys[number].player_ratings[conn.user.name] = conn.user.get_rating('lightning')
+        tourney.tourneys[number].players_in.append(conn.user.name)
+        conn.write("You have successfully joined tournament number %d!\n" % number)
+        tourney.tourneys[number].announce("%s(%d) has joined tournament #%d" %
+            (conn.user.name, tourney.tourneys[number].player_ratings[conn.user.name], number))
+        return
                 
 @ics_command('setpairingmethod', 'dw', admin.Level.user)
 class Setpairingmethod(Command):
@@ -68,6 +93,22 @@ class Setpairingmethod(Command):
             conn.write('"%s" is not a valid pairing method\n' % method)
             return
         tourney.tourneys[number].pairing_method = method
+
+@ics_command('settimecontrol', 'dS', admin.Level.user)
+class Settimecontrol(Command):
+    def run(self, args, conn):
+        number = args[0]
+        time_control = args[1]
+        if not conn.user.has_title('TM'):
+            conn.write("You are not a tournament manager (TM).\n")
+            return
+        if number >= len(tourney.tourneys):
+            conn.write('Tourney number %d not found.\n' % number)
+            return
+        if not len(time_control.split(" ")) == 2:
+            conn.write('That is not a valid time control.')
+        tourney.tourneys[number].time_control = time_control
+        conn.write('Time control of tourney number %d changed to "%s".\n' % (number, time_control))
 
 @ics_command('settourneyname', 'dS', admin.Level.user)
 class Settourneyname(Command):
